5e742ccc104b9d6b8acd488c261c4bcf0dc238d9
Rob Story <wrobstory@gmail.com>
2013-05-08 07:45:33 -0700

    Update setup.py
    Update to 0.1.5

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ required = (
 
 kw = {
     'name': 'vincent',
-    'version': '0.1.4',
+    'version': '0.1.5',
     'description': 'A Python to Vega translator',
     'long_description': open('README.rst', 'rt').read(),
     'author': 'Rob Story',
