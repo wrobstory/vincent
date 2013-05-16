43cd7d79d565f6c21163d22ad41c19f2d47e66c9
Dan Miller <dnmiller@gmail.com>
2013-05-15 19:13:09 -0700

    Adding scaffold for Data

--- vincent/core.py
+++ vincent/core.py
@@ -2,6 +2,16 @@
 
 import json
 
+try:
+    import pandas as pd
+except ImportError:
+    pd = None
+
+try:
+    import numpy as np
+except ImportError:
+    np = None
+
 
 def field_property(validator):
     """Decorator to define properties that map to the internal `_field`
@@ -31,3 +41,55 @@ def field_property(validator):
             del self._field[name]
 
     return property(getter, setter, deleter, validator.__doc__)
+
+
+def _assert_is_type(name, value, type):
+    """Assert that a value must be a given type."""
+    if not isinstance(value, type):
+        raise TypeError(name + ' must be a ' + str(type))
+
+
+class Data(object):
+
+    class LoadError(Exception):
+        """Exception for errors on loading data from third-party objects."""
+        pass
+
+    def __init__(self, name):
+        self._field = {}
+        self.name = name
+
+    @field_property
+    def name(value):
+        _assert_is_type('name', value, str)
+
+    @field_property
+    def url(value):
+        _assert_is_type('url', value, str)
+
+    @field_property
+    def values(value):
+        _assert_is_type('values', value, list)
+
+    @field_property
+    def source(value):
+        _assert_is_type('source', value, str)
+
+    def load_from(self, obj):
+        """Load values from an external object."""
+        if isinstance(obj, list):
+            self.values = obj
+        elif pd and isinstance(obj, (pd.Series, pd.DataFrame)):
+            self.load_from_pandas(obj)
+        elif np and isinstance(obj, np.ndarray):
+            self.load_from_numpy(obj)
+        else:
+            raise ValueError('unknown data type ' + str(type(obj)))
+
+    def load_from_pandas(self, pd_obj):
+        if not pd:
+            raise self.LoadError('pandas could not be imported')
+
+    def load_from_numpy(self, np_obj):
+        if not np:
+            raise self.LoadError('numpy could not be imported')
