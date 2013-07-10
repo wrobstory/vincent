5e302e5caf41f5b4dfc7b218cd9ffb17eef4c0b3
wrobstory <wrobstory@gmail.com>
2013-07-10 16:58:30 -0600

    Adding tests for vincent.charts.Bar

--- tests/test_charts.py
+++ tests/test_charts.py
@@ -8,7 +8,7 @@ Test Vincent.charts
 import numpy as np
 import pandas as pd
 import nose.tools as nt
-from vincent.charts import (data_type, Chart)
+from vincent.charts import (data_type, Chart, Bar)
 
 
 def test_data_type():
@@ -66,4 +66,34 @@ class TestChart(object):
         #Data loading errors
         nt.assert_raises(ValueError, Chart)
         nt.assert_raises(ValueError, Chart, [])
+
+class TestBar(object):
+    """Test Bar Chart"""
+
+    def test_init(self):
+        bar = Bar([1,2,3])
+
+        scales = [{u'domain': {u'data': u'table', u'field': u'data.x'},
+                   u'name': u'x',
+                   u'range': u'width',
+                   u'type': u'ordinal'}]
+
+        axes = [{u'scale': u'x', u'type': u'x'},
+                {u'scale': u'y', u'type': u'y'}]
+
+        marks = [{u'from': {u'data': u'table'},
+                  u'properties': {u'enter': {u'width': {u'band': True,
+                  u'offset': -1,
+                  u'scale': u'x'},
+                  u'x': {u'field': u'data.x', u'scale': u'x'},
+                  u'y': {u'field': u'data.y', u'scale': u'y'},
+                  u'y2': {u'scale': u'y', u'value': 0}},
+                  u'update': {u'fill': {u'value': u'steelblue'}}},
+                  u'type': u'rect'}]
+
+        for i, scale in enumerate(scales):
+            nt.assert_dict_equal(bar.scales[i].grammar(), scale)
+
+        for i, axis in enumerate(axes):
+            nt.assert_dict_equal(bar.axes[i].grammar(), axis)
 
