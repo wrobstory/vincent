a995d5d91dae25927ca6e47af69de462052abda2
wrobstory <wrobstory@gmail.com>
2013-07-10 21:10:16 -0600

    Git merge junk removal

--- vincent/__init__.py
+++ vincent/__init__.py
@@ -1,10 +1,8 @@
 # -*- coding: utf-8 -*-
 from vincent import Vega, Bar, Area, Scatter, Line, Map
-<<<<<<< HEAD
 from vega import (
     Data, Visualization, Scale, Mark, DataRef, Axis, MarkRef,
     MarkProperties, PropertySet, ValueRef, AxisProperties)
 from factories import (BarFactory)
 import charts
 from ipynb import init_d3, init_vg, display_vega
->>>>>>> d0d9a17f9b8b25681028fa43e3d6f7e9e1f0eeb9
