a6991cf31f885280266026ad99a2a276bd3199bb
Rob <wrobstory@gmail.com>
2013-07-28 22:30:47 -0700

    __init__.py change

--- vincent/__init__.py
+++ vincent/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import legacy
 from charts import (Chart, Bar, Line, Area, Scatter, StackedBar, StackedArea,
                     GroupedBar)
 from visualization import Visualization
