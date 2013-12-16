527f535e79d319f2b54b004e96b22c1de53b91a3
Rob <wrobstory@gmail.com>
2013-12-15 21:50:45 -0800

    Update to 0.4

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ required = ('pandas')
 
 kw = {
     'name': 'vincent',
-    'version': '0.3.0',
+    'version': '0.4.0',
     'description': 'A Python to Vega translator',
     'long_description': open('README.rst', 'rt').read(),
     'author': 'Rob Story',
